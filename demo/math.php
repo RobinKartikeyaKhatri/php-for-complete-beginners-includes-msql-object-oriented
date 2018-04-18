7a0fa83a6ac1507dba4abf78c5adef5f44ad1c8d
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2018-04-18 08:57:45 +0530

    created math.php file in demo folder

---
+++ demo/math.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Math in PHP</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    <?php
+    
+        // Math operations in PHP
+        echo 50 + 50;
+
+        echo "<br/>";
+
+        echo 10 - 10;
+        
+        echo "<br/>";
+
+        echo 20 * 3;
+
+        echo "<br/>";
+
+        echo 30 / 6;
+
+        echo "<br/>";
+
+        
+    
+    ?>
+</body>
+</html>
