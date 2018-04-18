b6d0f1cd7e4fe069d274434eb3a91921f0d629d5
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2018-04-18 10:21:36 +0530

    completed practice section 2

--- demo/_practical-app/2.php
+++ demo/_practical-app/2.php
@@ -21,18 +21,67 @@
 
 		  Step 2: Add the two variables and display the sum with echo:
 
-
 		  Step3: Make 2 Arrays with the same values, one regular and the other associative
 
 		  Step4: Make a constant and set it to the value of PHP. and use an echo to print it out
 
+		 */
 
-			
+		 $number1 = 10;
+		 $number2 = 20;
 
-			 */
+		 echo $number1 + $number2;
 
-		
+		 $regularArray = array(10, 20, 30, 40, 50, 60, 70, 80, 90, 100);
+		 
+		 $associativeArray = array(
+			 'number_one' => 10, 
+			 'number_two' => 20,
+			 'number_three' => 30,
+			 'number_four' => 40,
+			 'number_five' => 50,
+			 'number_six' => 60,
+			 'number_seven' => 70,
+			 'number_eight' => 80,
+			 'number_nine' => 90,
+			 'number_ten' => 100
+		 );
 
+		 echo "<br/><br/><br/>";
+
+		 var_dump($regularArray);
+
+		 echo "<br/><br/><br/>";
+
+		 print_r($regularArray);
+
+		 echo "<br/><br/>";
+
+		 foreach($regularArray as $values)
+		 {
+			 echo $values . "<br/>";
+		 }
+
+		 echo "<br/><br/>";
+
+		 var_dump($associativeArray);
+
+		 echo "<br/><br/><br/>";
+
+		 print_r($associativeArray);
+
+		 echo "<br/><br/>";
+
+		 foreach ($associativeArray as $key => $values) 
+		 {
+			 echo $key . " : " . $values . "<br/>";
+		 }
+
+		 echo "<br/><br/>";
+
+		 define('NAME', 'Robin Kartikeya Khatri');
+
+		 echo NAME;
 
 		?>
 
