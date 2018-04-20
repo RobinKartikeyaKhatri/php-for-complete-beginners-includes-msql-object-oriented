3d8a748f88223193c068334d9e005c026d8987f4
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2018-04-20 23:22:22 +0530

    create updateUsers() function in login_update.php file

--- demo/mysql/functions.php
+++ demo/mysql/functions.php
@@ -16,4 +16,29 @@ function showAllData()
     }
 }
 
+function updateUsers()
+{
+    global $conn;
+    
+    if (isset($_POST['update'])) 
+    {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $id = $_POST['id'];
+
+        $query = "UPDATE users SET username = '$username', password = '$password' WHERE id = $id";
+        $result = mysqli_query($conn, $query);
+
+        if ($result) 
+        {
+            echo "<p class='text-success text-center'>Recored updated successfuly.</p>";
+        }
+        else
+        {
+            echo "<p class='text-danger text-center'>Unable to update record</p>";
+            die("Query failed!" . mysqli_error($conn));
+        }
+    }
+}
+
 ?>
