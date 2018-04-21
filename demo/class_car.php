723fa864010461c01104d7e4ddbe9e1fe4853c72
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2018-04-21 18:58:13 +0530

    learned how to create and use static properties and methods in class

--- demo/class_car.php
+++ demo/class_car.php
@@ -2,52 +2,18 @@
 
 class Car
 {
+    static $wheels = 4;
+    var $hood = 1;
 
-    public $wheels = 4;
-    private $hood = 1;
-    protected $engine = 1;
-    //$doors = 4;
-
-    function showProperties()
+    function showStaticProperty()
     {
-        echo $this->wheels . "<br/>";
-        echo $this->hood . "<br/>";
-        echo $this->engine . "<br/>";
-    }
-
-    function showPrivateProperty()
-    {
-        echo $this->hood . "<br/>";
+        echo self::$wheels . "<br/>";
     }
 }
 
-$bmw = new Car();
+echo Car::$wheels . "<br/>";
 
-$bmw->showProperties();
-
-echo "<br/><br/>";
-
-echo $bmw->wheels . "<br/>";
-//echo $bmw->hood . "<br/>";
-//echo $bmw->engine . "<br/>";
-
-class Truck extends Car
-{
-    function showEngineProperty()
-    {
-        echo $this->engine . "<br/>";
-    }
-}
-
-$tacoma = new Truck;
-
-$tacoma->showEngineProperty();
-
-echo "<br/><br/>";
-
-$bmw->showPrivateProperty();
-
-
+Car::showStaticProperty();
 
 
 ?>
