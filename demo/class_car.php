ec3ad5f92ca11765335a6f2c02f896164e6c59a5
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2018-04-21 17:57:34 +0530

    created properties in class and learnd how to use them via class instance

--- demo/class_car.php
+++ demo/class_car.php
@@ -2,16 +2,27 @@
 
 class Car
 {
+
+    var $wheels = 4;
+    var $hood = 1;
+    var $engine = 1;
+    var $doors = 4;
+
     function moveWheels()
     {
-        echo "Wheels move";
+        $this->wheels = 8;
     }
 }
 
 $bmw = new Car();
-$tacoma = new Car();
 
-$bmw->moveWheels();
-$tacoma->moveWheels();
+//$bmw->wheels = 8; // assign a new value to class property via object of the class
+
+echo $bmw->wheels . "<br/>";
+echo $bmw->hood . "<br/>";
+echo $bmw->engine . "<br/>";
+echo $bmw->doors . "<br/>";
+
+
 
 ?>
