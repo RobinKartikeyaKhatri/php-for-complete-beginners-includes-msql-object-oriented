7e9a864a0ee372310e4d35506e99ea1137e5b857
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2018-04-21 10:12:14 +0530

    refector code in login_read.php file

--- demo/mysql/login_read.php
+++ demo/mysql/login_read.php
@@ -1,34 +1,15 @@
+<?php include("db.php"); ?>
+<?php include("functions.php"); ?>
+
 <?php include("includes/header.php"); ?>
 
     <div class="container">
         <div class="col-sm-6">
         <h1 class="text-center">Show Users</h1>
-            <?php
-
-                $conn = mysqli_connect('localhost', 'root', '', 'loginapp');
-
-                $query = "SELECT * FROM users";
-
-                $result = mysqli_query($conn, $query);
-
-                while ($row = mysqli_fetch_array($result)) 
-                {
-            ?>
-                <pre>
-                <?php
-                    echo $id = $row['id'] . "<br/>";
-                    echo $username   = $row['username'] . "<br/>";
-                    echo $password   = $row['password'] . "<br/>";
-                ?>
-                </pre>
-            <?php  
-                }
-            ?>
-
-
-
-
-        </div>
+        <pre>
+            <?php readUser(); ?>
+        </pre>
+    </div>
     </div>
         
 
